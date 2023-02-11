a5d9eddb3bd348e8d4ce6186120945a1fe92a3c3
facupalopoli <facupalopoli@gmail.com>
2023-02-11 16:34:42 -0300

    3er commit

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 import logo from '../../assets/logoPortfolio.png'
 
 const Navbar = () => {
+    const [menuToggle, setMenuToggle] = useState(false);
+
     return(
         <header className='header'>
             <img src={logo} alt="logo" />
-            <div className='navBar'>
-                <a href='#'><span>Ho</span>me</a>
-                <a href='#about'><span>Ac</span>erca</a>
-                <a href='#projects'><span>Pro</span>yectos</a>
-                <a href='#testimonials'><span>Tes</span>timonios</a>
-                <a href='#contact'><span>Co</span>ntacto</a>
+            <div className={`navBar ${menuToggle && 'open'}`}>
+                <a href='#' onClick={()=> setMenuToggle(!menuToggle)}><span>Ho</span>me</a>
+                <a href='#about' onClick={()=> setMenuToggle(!menuToggle)}><span>Ac</span>erca</a>
+                <a href='#projects' onClick={()=> setMenuToggle(!menuToggle)}><span>Pro</span>yectos</a>
+                <a href='#testimonials' onClick={()=> setMenuToggle(!menuToggle)}><span>Tes</span>timonios</a>
+                <a href='#contact' onClick={()=> setMenuToggle(!menuToggle)}><span>Co</span>ntacto</a>
             </div>
-            <i className="bi bi-list hamburguesa"></i>
+            <i className={menuToggle ? `bi bi-x-lg cruz` : `bi bi-list hamburguesa`} onClick={()=> setMenuToggle(!menuToggle)}></i>
         </header>
     )
 }
