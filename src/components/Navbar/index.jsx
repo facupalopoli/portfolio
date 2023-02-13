adfa08ab06e3f0ec94e9e909da63e5fe1ccf7289
facupalopoli <facupalopoli@gmail.com>
2023-02-13 00:43:46 -0300

    5to commit

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -9,11 +9,11 @@ const Navbar = () => {
         <header className='header'>
             <img src={logo} alt="logo" />
             <div className={`navBar ${menuToggle && 'open'}`}>
-                <a href='#' onClick={()=> setMenuToggle(!menuToggle)}><span>Ho</span>me</a>
-                <a href='#about' onClick={()=> setMenuToggle(!menuToggle)}><span>Ac</span>erca</a>
-                <a href='#projects' onClick={()=> setMenuToggle(!menuToggle)}><span>Pro</span>yectos</a>
-                <a href='#testimonials' onClick={()=> setMenuToggle(!menuToggle)}><span>Tes</span>timonios</a>
-                <a href='#contact' onClick={()=> setMenuToggle(!menuToggle)}><span>Co</span>ntacto</a>
+                <a className='enlaces' href='#' onClick={()=> setMenuToggle(!menuToggle)}>Home</a>
+                <a className='enlaces' href='#about' onClick={()=> setMenuToggle(!menuToggle)}>Acerca</a>
+                <a className='enlaces' href='#projects' onClick={()=> setMenuToggle(!menuToggle)}>Proyectos</a>
+                <a className='enlaces' href='#testimonials' onClick={()=> setMenuToggle(!menuToggle)}>Testimonios</a>
+                <a className='enlaces' href='#contact' onClick={()=> setMenuToggle(!menuToggle)}>Contacto</a>
             </div>
             <i className={menuToggle ? `bi bi-x-lg cruz` : `bi bi-list hamburguesa`} onClick={()=> setMenuToggle(!menuToggle)}></i>
         </header>
